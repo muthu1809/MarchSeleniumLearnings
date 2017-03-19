cfbc194d37ea36aaf741514aab4e1facae53043c
Muthu <muthu1809@gmail.com>
2017-03-19 09:11:24 +0530

    Editing program

--- src/com/payilagam/javabasics/MyFirstJavaProgram.java
+++ src/com/payilagam/javabasics/MyFirstJavaProgram.java
@@ -3,7 +3,7 @@ package com.payilagam.javabasics;
 public class MyFirstJavaProgram {
 	static int a; 
 	public static void main(String[] args) {
-	
+	System.out.println("a is "+ a);
 	System.out.println("a is "+ a);
 	}
 }
